9197f63055a53214c0a8b8ac04cc6554143aea6b
Chan Weng Ho <cwengho@yahoo.com>
2021-08-01 15:14:09 +0800

    made html consistent and generalized js funtion

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-document.getElementById("buy2").addEventListener("mouseover", buy2);
-document.getElementById("buy2").addEventListener("mouseout", buy2out);
+"use strict"
 
-function buy2() {
-    document.getElementById("switch2").setAttribute("href","https://www.google.com/");
+var buyList = document.getElementsByClassName("buy")
+
+for (var i = 0; i < buyList.length; i++) {
+    let z = buyList[i]
+    let w = z.getAttribute("data-revert")
+    let x = z.getAttribute("data-buy");
+    let y = z.getAttribute("data-idref");
+    z.addEventListener("mouseover", function() {buy(x,y)});
+    z.addEventListener("mouseout", function() {revert(w,y)});
 }
 
-function buy2out() {
-    document.getElementById("switch2").setAttribute("href","");
+function buy(link,id) {
+    document.getElementById(id).setAttribute("href",link);
+}
+
+function revert(link,id) {
+    document.getElementById(id).setAttribute("href",link);
 }
